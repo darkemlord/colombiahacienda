55f70af8d51ecc9c069c04f21b648fb301b3b31d
emanuel caro <darkemlord@gmail.com>
2022-01-05 23:47:11 +0900

    working spaceupdated and connected with the shopify app

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Heading>Shopify app with Node and React Por fin los amo a todos! 🎉</Heading>
   </Page>
 );
 
