901b0cdafe3d5fd8296514953d83e8a647996713
emanuel caro <darkemlord@gmail.com>
2022-01-06 09:33:45 +0900

    action button working and react imported to handle the state button

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
 import { EmptyState, Layout, Page } from "@shopify/polaris";
+import React, { useState } from 'react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
-  <Page>
+const Index = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <Page>
     <Layout>
       <EmptyState // This is an empty state, it is used to create new components through polaris
         heading='Manage Your Inventory Transfers'
         action={{content: 'add transfer',
-        onAction: () => console.log('hello ema')
-        }}
+        onAction: () => {
+          setOpen(true);
+          console.log(open)
+        }
+      }}
       >
       </EmptyState>
     </Layout>
   </Page>
 );
+}
 
 export default Index;
