0c27a9694bd285f64a6b66f4123bf928c30a1c13
emanuel caro <darkemlord@gmail.com>
2022-01-06 12:32:28 +0900

    importations done

---
+++ pages/components/ResourceListWithProducts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Card, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polaris';
+import store from 'store-js';
+import { Redirect } from '@shopify/app-bridge/actions';
+import { Context } from '@shopify/app-bridge-react';
+
+export const ResourceListWithProducts = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
