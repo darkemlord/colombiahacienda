630627210eb0a37e1a5411940183197f62affa15
emanuel caro <darkemlord@gmail.com>
2022-01-06 10:31:47 +0900

    select products button worling

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,35 @@ import { EmptyState, Layout, Page } from "@shopify/polaris";
 import React, { useState } from 'react';
 import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
 
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
 // the version of react that works with functions is used, changes have been made to the original syntax to make it work.
 const Index = () => {
   const [open, setOpen] = useState(false) //handle the image src
-
+  const handleSelection = (resources) => {
+    setOpen(false);
+    console.log(resources)
+  }
   return (
     <Page>
     <Layout>
+      <TitleBar
+        primaryAction={{
+          content: 'Select Products',
+          onAction: () => {
+            setOpen(true);
+          }
+        }}
+      />
+      <ResourcePicker
+        resourceType='Product'
+        showVariants={false}
+        open={open}
+        onSelection={(resources) => {
+          handleSelection(resources)
+        }}
+        onCancel={() => setOpen(false)}
+      />
       <EmptyState // This is an empty state, it is used to create new components through polaris
         heading='Manage Your Inventory Transfers'
         action={{content: 'Handle image',
@@ -16,7 +38,7 @@ const Index = () => {
           console.log('hello world')
         }
       }}
-        image={image}
+        image={img}
       >
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, temporibus.</p>
       </EmptyState>
@@ -24,5 +46,6 @@ const Index = () => {
   </Page>
 );
 }
+
 
 export default Index;
