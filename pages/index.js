fc6763c8920fe9185e9835a42a99ee85d90b0016
emanuel caro <darkemlord@gmail.com>
2022-01-06 09:16:17 +0900

    creating an empty state through polaris

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import { Heading, Page } from "@shopify/polaris";
+import { EmptyState, Heading, Layout, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React Ema programa! 🎉</Heading>
+    <Layout>
+      <EmptyState // This is an empty state, it is used to create new components through polaris
+        heading='Manage Your Inventory Transfers'
+        action={{content: 'add transfer',
+        onAction: () => this.setState({open:true})
+        }}
+      >
+      </EmptyState>
+    </Layout>
   </Page>
 );
 
