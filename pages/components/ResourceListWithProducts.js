b6f98541596fafb60d6072a7f8ae9369b71d508d
emanuel caro <darkemlord@gmail.com>
2022-01-06 15:07:15 +0900

    graphql query created

--- pages/components/ResourceListWithProducts.js
+++ pages/components/ResourceListWithProducts.js
@@ -6,6 +6,37 @@ import store from 'store-js';
 import { Redirect } from '@shopify/app-bridge/actions';
 import { Context } from '@shopify/app-bridge-react';
 
+// This is the graphql query
+
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              id
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const ResourceListWithProducts = () => {
   console.log(store)
   return (
